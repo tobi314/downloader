e689136879a53a058b834291fab235425ef69fc0
Tobias Jeltsch <tobias@jeltsch.org>
2021-03-01 11:14:17 +0200

    rewrote main.py

--- main.py
+++ main.py
@@ -7,63 +7,76 @@ import click
 import time
 import os
 
-USERNAME, PASSWORD = "mjeltsch", "***REMOVED***"
+USERNAME, PASSWORD, URL = "mjeltsch", "***REMOVED***", "https://workgroups.helsinki.fi/display/IP/Workshop+4"#"https://workgroups.helsinki.fi/display/TS/Sana+on+vapaa"
+DOWNLOAD_DIR = "/home/tobias/Desktop/test_downloads"
 
-def login(driver):
-	driver.get(r"https://workgroups.helsinki.fi/loginout/HYcrowdlogin.php?url=%2Fpages%2Fviewpage.action%3FspaceKey%3DMATRANSMED%26title%3DAPPLICATIONS%2B-%2BHAKEMUKSET")
+
+def login(driver, url):
+	driver.get(url)
+	driver.find_element_by_id("default_login").click()
 	driver.find_element_by_id("username").send_keys(USERNAME)
 	driver.find_element_by_id("password").send_keys(PASSWORD)
 	driver.find_element_by_class_name("form-button").click()
 
-def download_files(driver, cutoff):
-	#Find links
-	links= []
-	elements = driver.find_elements_by_css_selector("#rw_pagetree_item_74300140 .rw_item_content a")
+def get_pages(driver):
+	driver.implicitly_wait(2)
+	lists = driver.find_elements_by_css_selector("ul.rw_pagetree_list")
+	for _list in lists:
+		if driver.find_element_by_class_name("rw_current_page_item"):
+			link_container = _list
+
+	elements = link_container.find_elements_by_css_selector("ul.rw_pagetree_list a")
+
+	links = []
 	for element in elements:
 		links.append(element.get_attribute("href"))
 
-	#Download files
+	return links
+
+
+def download_files(driver, pages, cutoff):
 	i = 0
-	with click.progressbar(links, label="Downloading files...") as _links:
-		for link in _links:
-			driver.get(link)
-			for file in driver.find_elements_by_class_name("filename"):
-				file.click()
+	for page in pages:
+		driver.get(page)
+		for link in driver.find_elements_by_css_selector('a[href^="/download"]'):
+			link.click()
+			driver.find_element_by_css_selector('a[aria-label="Download"]').click()
+			driver.find_element_by_css_selector('button[aria-label="Close"]').click()
 			i += 1
+
 			if i > cutoff:
 				print("\nCutoff point reached, aborting programm")
-				break
+				return i
+
+	return i
+
+def wait_for_downloads(driver, dir, num_downloads, num_existing_files):
+	while True:
+		files = os.listdir(dir)
+		d = [file for file in files if file.endswith(".crdownload")]
+		l = len(files)
+		print(d, l)
+		if d or l != num_existing_files + num_downloads:
+			driver.implicitly_wait(1)
+		else:
+			break
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-v", "--version", action="version", version="FILE_DOWNLOADER 1.0")
-	parser.add_argument("-d", type=str, default=os.getcwd(), help="specify download directory, default: working directory")
-	parser.add_argument("-m", type=int, default=10000, help="specify max amount of files to download")
-	parser.add_argument("-l", default=False, action="store_const", const=True, help="make webdriver run headless")
-	args = parser.parse_args()
-
-	print("Preparing Download...")
 	options = webdriver.ChromeOptions()
-	options.add_experimental_option("prefs", {"download.default_directory": args.d,
+	options.add_experimental_option("prefs", {"download.default_directory": DOWNLOAD_DIR,
 											  "download.prompt_for_download": False,
   											  "download.directory_upgrade": True,
   											  "safebrowsing.enabled": True})
-	if args.l:
+
+	if False:
 		options.add_argument('--headless')
 
+	m = len(os.listdir(DOWNLOAD_DIR))
 	driver = webdriver.Chrome(options=options)
-	driver.implicitly_wait(5)
-
-	try:
-		login(driver)
-		download_files(driver, args.m)
-		time.sleep(3)
-		driver.quit()
-
-	except Exception as e:
-		driver.save_screenshot("error.png")
-		driver.quit()
-		raise e
+	login(driver, URL)
+	pages = get_pages(driver)
+	n = download_files(driver, pages, 5)
+	wait_for_downloads(driver, DOWNLOAD_DIR, n, m)
 
 if __name__ == "__main__":
 	main()
